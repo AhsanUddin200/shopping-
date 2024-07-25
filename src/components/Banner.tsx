d5accedf9c13923090929058374377f1058ba40e
Ahsan Nasir <ahsanuddin160@gmail.com>
2024-07-25 17:33:51 +0500

    Install this two packages for slider npm install react-slick --save

---
+++ src/components/Banner.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Banner = () => {
+    var settings = {
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        SlidesToScroll: 1,
+        pauseOnHover: false,
+        autoplay: true
+    }
+  return (
+    
+    <>
+    <div>
+        <div>
+
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Banner
