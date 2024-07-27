b00f34e451849e940a64b5418e26e09de93e0bd4
Ahsan Nasir <ahsanuddin160@gmail.com>
2024-07-27 22:48:16 +0500

    Work on Product

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Image from 'next/image';
+
+interface PropsType {
+  img: string;
+  title: string;
+  desc: string;
+  rating: number;
+  price: string;
+}
+
+const ProductCard: React.FC<PropsType> = ({ img, title, desc, rating, price }) => {
+  return (
+    <div className='px-4 border border-gray-400 rounded-lg max-w-[800px]'>
+      <Image className="w-full h-auto" src={img} width={200} height={300} alt={title} />
+      
+    </div>
+  );
+}
+
+export default ProductCard;
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,55 +1,56 @@
 import React from "react";
+import ProductCard from "./ProductCard";
 
 const Products = () => {
   const itemsdata = [
     {
       img: "/photo1.png",
       title: "Cloths",
-      decs: "Baby Frog -White",
+      desc: "Baby Frog -White",
       rating: 5,
       price: "$5.90",
     },
-
     {
       img: "/photo2.png",
       title: "Watch",
-      decs: "Smart Watch For Man",
+      desc: "Smart Watch For Man",
       rating: 2,
       price: "$9.00",
     },
-
     {
       img: "/photo3.png",
       title: "Cloths",
-      decs: "Pant",
+      desc: "Pant",
       rating: 9,
       price: "$3.90",
+    },
+    {
+      img: "/photo4.png",
+      title: "Cloths",
+      desc: "Pant And Shirt",
+      rating: 10,
+      price: "$9.90",
     },
   ];
 
   return (
-    <>
-      <div className="container pt-16">
-        <div className="font-serif text-3xl pb-4 text-gray-400">
-          Our Products
-        </div>
-
-        <div className="grid grid-cols-1 place-items-center sm:place-items-start sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 xl:gap-x-20 xl:gap-y-20 ">
-
-         {
-            itemsdata.map((items,index)=>(
-                <ProductCard 
-                key={index}
-                img={items.img}
-                title={items.title}
-                decs={items.decs}
-                rating={items.rating}
-                price={items.price} />
-            ))
-         }
-        </div>
+    <div className="container pt-16">
+      <div className="font-serif text-3xl pb-4 text-gray-400">
+        Our Products
       </div>
-    </>
+      <div className="grid grid-cols-1 place-items-center sm:place-items-start sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 xl:gap-x-20 xl:gap-y-20">
+        {itemsdata.map((item, index) => (
+          <ProductCard
+            key={index}
+            img={item.img}
+            title={item.title}
+            desc={item.desc}
+            rating={item.rating}
+            price={item.price}
+          />
+        ))}
+      </div>
+    </div>
   );
 };
 
