885b9d1631d06dbaa4c34bb8cba95b1b415a0ccb
Ahsan Nasir <ahsanuddin160@gmail.com>
2024-07-30 17:38:08 +0500

    Complete Products

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
+import { AiFillStar } from "react-icons/ai";
+import { AiOutlineStar } from "react-icons/ai";
 
 interface PropsType {
   img: string;
@@ -9,13 +11,102 @@ interface PropsType {
   price: string;
 }
 
-const ProductCard: React.FC<PropsType> = ({ img, title, desc, rating, price }) => {
+const generateRating = (rating: number) => {
+  switch (rating) {
+    case 1:
+      return (
+        <div className="flex gap-2 text-[25px] text-yellow-500">
+          <AiFillStar />
+          <AiOutlineStar />
+          <AiOutlineStar />
+          <AiOutlineStar />
+          <AiOutlineStar />
+        </div>
+      );
+
+    case 2:
+      return (
+        <div className="flex gap-none text-[25px]  text-yellow-500">
+          <AiFillStar />
+          <AiFillStar />
+          <AiOutlineStar />
+          <AiOutlineStar />
+          <AiOutlineStar />
+        </div>
+      );
+
+    case 3:
+      return (
+        <div className="flex gap-1 text-[25px]  text-yellow-500">
+          <AiFillStar />
+          <AiFillStar />
+          <AiFillStar />
+          <AiOutlineStar />
+          <AiOutlineStar />
+        </div>
+      );
+
+    case 4:
+      return (
+        <div className="flex gap-2 text-[25px]  text-yellow-500 ">
+          <AiFillStar />
+          <AiFillStar />
+          <AiFillStar />
+          <AiFillStar />
+          <AiOutlineStar />
+        </div>
+      );
+
+    case 5:
+      return (
+        <div className="flex gap-2 text-[25px]  text-yellow-500 ">
+          <AiFillStar />
+          <AiFillStar />
+          <AiFillStar />
+          <AiFillStar />
+          <AiFillStar />
+        </div>
+      );
+    default:
+      return null;
+  }
+};
+
+const ProductCard: React.FC<PropsType> = ({
+  img,
+  title,
+  desc,
+  rating,
+  price,
+}) => {
   return (
-    <div className='px-4 border border-gray-400 rounded-lg max-w-[800px]'>
-      <Image className="w-[500px] h-[300px] " src={img} width={200} height={300} alt={title} />
-      
+    <div className="text-gray-600 text-center text-xl ">
+      <div className="font-bold text-2xl ">{title}</div>
+
+      <div className="px-4 border border-gray-400 rounded-lg max-w-[800px]">
+        <div>
+          <Image
+            className="w-[500px] h-[300px] hover:blur-sm"
+            src={img}
+            width={200}
+            height={300}
+            alt={title}
+          />
+          <button className="font-semibold text-lg bg-red-600 rounded-md mt-4 mb-4 px-4 text-white hover:text-black hover:bg-white">
+            Shop Now{" "}
+          </button>
+        </div>
+        <div className="py-4 space-y-2 px-12">
+          <p className="text-gray-500">{desc}</p>
+         <div>{generateRating(rating)}</div>
+         <div className="font-semibold flex gap-4">
+          ${price}
+          <del className="text-gray-00 font-sm">${parseInt(price) + 5}.00</del>
+         </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default ProductCard;
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -7,21 +7,21 @@ const Products = () => {
       img: "/photo1.png",
       title: "Cloths",
       desc: "Baby Frog -White",
-      rating: 5,
+      rating: 3,
       price: "$5.90",
     },
     {
       img: "/photo2.png",
       title: "Watch",
       desc: "Smart Watch For Man",
-      rating: 2,
+      rating: 3,
       price: "$9.00",
     },
     {
       img: "/photo3.png",
       title: "Cloths",
       desc: "Pant",
-      rating: 9,
+      rating: 4,
       price: "$3.90",
     },
    
@@ -29,14 +29,22 @@ const Products = () => {
       img: "/photo7.jpeg",
       title: "Cloths and Kapray",
       desc: "Pant And Shirt",
-      rating: 10,
+      rating: 2,
+      price: "$9.90",
+    },
+
+    {
+      img: "/photo6.jpg",
+      title: "Cloths and Kapray",
+      desc: "Pant And Shirt",
+      rating:5,
       price: "$9.90",
     },
   ];
 
   return (
     <div className="container pt-16">
-      <div className="font-serif text-3xl pb-4 text-gray-400">
+      <div className="font-serif text-3xl pb-4 text-gray-400 text-center">
         Our Products
       </div>
       <div className="grid grid-cols-1 place-items-center sm:place-items-start sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 xl:gap-x-20 xl:gap-y-20 px-4 mt-6 mb-8">
