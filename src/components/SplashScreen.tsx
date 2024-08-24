a2df3a831608ca103146490a8f5c173bf0debbce
Ahsan Nasir <ahsanuddin160@gmail.com>
2024-08-24 16:20:35 +0500

    Update Splash Screen

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -34,12 +34,12 @@ function SplashScreen({ finishLoading }: { finishLoading: () => void }) {
   <Image
     src={logo1}
     alt="Brand Logo"
-    className="px-8 w-full hover:scale-150"
+    className="px-8 w-full hover:scale-150 blur-sm"
   />
-  <div role="status" className="absolute inset-0 flex justify-center items-center">
+  <div role="status" className="absolute inset-0 flex justify-center items-center ">
     <svg
       aria-hidden="true"
-      className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+      className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-800 fill-yellow-700"
       viewBox="0 0 100 101"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
