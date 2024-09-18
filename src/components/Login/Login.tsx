bf1a3af17172d0dffd63aad399c58839f7dbdd77
Ahsan nasir <engrnabeel.talib@gmail.com>
2024-09-18 22:46:17 +0500

    Login

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,19 +1,15 @@
-import React from 'react'
+import React from "react";
 
 export default function Login() {
   return (
     <>
-    <div>
-
-      {/* Div left sdie */}
-      <div>
-        </div>
-{/* Div right side sdie */}
-<div>
-        </div>
-    </div>
+      <div className="flex flex-col items-center w-full flex-1 px-20 text-center">
+       
+        <div
+        className="bg-white">Sign in</div>
+       
+        <div></div>
+      </div>
     </>
-
-
-  )
+  );
 }
