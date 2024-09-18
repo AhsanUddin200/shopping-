29bfb194bd1706f34090d087c723b38a21805856
Ahsan nasir <engrnabeel.talib@gmail.com>
2024-09-19 02:17:44 +0500

    Login

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -4,10 +4,8 @@ export default function Login() {
   return (
     <>
       <div className="flex flex-col items-center w-full flex-1 px-20 text-center">
-       
-        <div
-        className="bg-white">Sign in</div>
-       
+        <div className="bg-white">Sign in</div>
+
         <div></div>
       </div>
     </>
