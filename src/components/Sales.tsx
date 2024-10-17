52fba07e96941558cd3cae98391cf568716408ad
Ahsan nasir <engrnabeel.talib@gmail.com>
2024-10-17 22:22:31 +0500

    Slaes page redesign

--- src/components/Sales.tsx
+++ src/components/Sales.tsx
@@ -1,15 +1,12 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
-import sale1 from "../../public/sale1.jpeg";
-import sale2 from "../../public/sale2.jpg";
-import { useState, useEffect } from "react";
 
 export default function Sales() {
   const [visible, setVisible] = useState(false);
 
   useEffect(() => {
-    const showTimeOut = setTimeout(() => {
+    const showTimeout = setTimeout(() => {
       setVisible(true);
     }, 3000);
 
@@ -18,160 +15,190 @@ export default function Sales() {
     }, 9000);
 
     return () => {
-      clearTimeout(showTimeOut);
+      clearTimeout(showTimeout);
       clearTimeout(hideTimeout);
     };
-  });
+  }, []);
 
   return (
-    <>
-    <div className="dark:bg-dark min-h-screen ">
-      <div
-        id="SalesOffer"
-        className="font-serif text-3xl dark:text-white text-gray-400 text-center dark:bg-dark w-full "
-      >
-        Sales Offer
+    <div className="min-h-screen bg-white dark:bg-gray-900 py-16">
+      <div   id="SalesOffer" className="text-center mb-12 ">
+        <h1 className="text-5xl font-extrabold text-gray-800 dark:text-white mb-6">
+          Exclusive Sales
+        </h1>
+        <p className="text-xl text-gray-600 dark:text-gray-400">
+          Best deals for Men, Women, and Kids
+        </p>
       </div>
-      <div className="text-center text-sm  dark:text-white"> Men,Woman,Boys & Girls</div>
-
-      <div className="dark:bg-dark  flex px-8 bg-gray-300 sm:px-4 flex-col sm:flex-row sm:mt-8 justify-center items-center rounded-2xl shadow-2xl w-full min-h-screen   ">
+      <div className="container mx-auto px-4">
         {visible && (
-          <div className=" bg-red-600  w-25 text-gray-400 text-center px-3 sm:px-0 rounded-b-2xl sm:rounded-e-2xl font-bold text-xl h-full">
-            <p>Flat 50% off</p>
+          <div className="bg-red-600 text-white text-center py-4 rounded-md mb-8">
+            <p className="text-3xl font-bold">Flat 50% off</p>
           </div>
         )}
-
-        <div className="flex md:flex-row sm:flex-col gap-8 ">
-          <div className=" mt-24 sm:mt-12 sm:px-16 lg:px-16 ">
+        <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {/* Sale Item 1 */}
+          <div className="relative group bg-gray-100 dark:bg-gray-800 rounded-lg shadow-lg overflow-hidden">
             <Image
               src="https://images.unsplash.com/photo-1631005438015-a2d58390d01e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               alt="sale offer"
+              layout="responsive"
               width={500}
-              height={200}
-              className="w-full sm:w-auto rounded-md hover:blur-sm "
+              height={300}
+              className="object-cover w-full h-64 transition-transform duration-300 group-hover:scale-110"
             />
-            <div className="flex gap-4 ">
-              <del className="mt-2 text-center text ml-10 sm:ml-32 dark:text-white">$126</del>
-              <p className="mt-2 text-center font-bold dark:text-white">${parseInt(126 / 2)}</p>
-            </div>
-            <button className="border border-gray-400  hover:border-transparent px-3 dark:text-white rounded-md hover:bg-red-500 text-black hover:text-white ml:[66px] sm:ml-[140px]">
-              Shop
-            </button>
-          </div>
-
-          <div className="flex flex-col justify-center items-center gap-6 mt-1">
-            <div className="flex flex-col sm-flex-row items-center">
-              <Image
-                className=" rounded-md "
-                src="https://images.pexels.com/photos/7139722/pexels-photo-7139722.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                alt="sale offer"
-                width={200}
-                height={100}
-              />
-
-              <div className="flex gap-4">
-                <del className="mt-2 text-center text dark:text-white">$55</del>
-                <p className="mt-2 text-center font-bold dark:text-white">
-                  ${parseInt(55 / 2)}
-                </p>
+            <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent p-4">
+              <div className="flex justify-between items-center mb-4">
+                <del className="text-gray-400 dark:text-gray-500">$126</del>
+                <p className="text-2xl font-bold text-white">$63</p>
               </div>
-              <button className="border border-gray-400  hover:border-transparent px-3 rounded-md dark:text-white hover:bg-red-500 text-black hover:text-white">
-                Shop
-              </button>
-            </div>
-            <div className="flex flex-col items-center">
-              <Image
-                className="rounded-md "
-                src="https://images.pexels.com/photos/18571715/pexels-photo-18571715/free-photo-of-child-model-posing-in-bow-tie-and-suspenders.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                alt="sale offer"
-                width={200}
-                height={100}
-              />
-              <div className="flex gap-4">
-                <del className="mt-2 text-center text dark:text-white ">$67</del>
-                <p className="mt-2 text-center font-bold dark:text-white">
-                  ${parseInt(67 / 2)}
-                </p>
-              </div>
-              <button className="border border-gray-400  hover:border-transparent px-3 dark:text-white rounded-md hover:bg-red-500 text-black hover:text-white">
-                Shop
+              <button className="w-full py-2 bg-red-500 text-white rounded hover:bg-red-700 transition-colors">
+                Shop Now
               </button>
             </div>
           </div>
-
-          <div className="hidden sm:flex flex-col sm:flex-row sm:px-12 justify-center items-center gap-12 mt-1">
-            <div className="flex flex-col items-center max-w-xs sm:max-w-sm">
-              <Image
-                className="w-24 sm:w-32 h-32 sm:h-48 object-cover rounded-md"
-                src="https://images.pexels.com/photos/19664801/pexels-photo-19664801/free-photo-of-little-boy-in-vest-and-white-shirt-standing-on-rock.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                alt="sale offer"
-                width={200}
-                height={100}
-              />
-              <div className="flex gap-4 mt-2 text-center">
-                <del className="text-gray-500 dark:text-white">$100</del>
-                <p className="font-bold dark:text-white">${parseInt(100 / 2)}</p>
+          {/* Sale Item 2 */}
+          <div className="relative group bg-gray-100 dark:bg-gray-800 rounded-lg shadow-lg overflow-hidden">
+            <Image
+              src="https://images.pexels.com/photos/7139722/pexels-photo-7139722.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="sale offer"
+              layout="responsive"
+              width={500}
+              height={300}
+              className="object-cover w-full h-64 transition-transform duration-300 group-hover:scale-110"
+            />
+            <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent p-4">
+              <div className="flex justify-between items-center mb-4">
+                <del className="text-gray-400 dark:text-gray-500">$55</del>
+                <p className="text-2xl font-bold text-white">$27.5</p>
               </div>
-              <button className="mt-2 border border-gray-400 hover:border-transparent px-3 dark:text-white  rounded-md hover:bg-red-500 text-black hover:text-white">
-                Shop
+              <button className="w-full py-2 bg-red-500 text-white rounded hover:bg-red-700 transition-colors">
+                Shop Now
               </button>
             </div>
-
-            <div className="flex flex-col items-center max-w-xs sm:max-w-sm">
-              <Image
-                className="w-24 sm:w-72 h-32 sm:h-96 rounded-md"
-                src="https://images.pexels.com/photos/20777176/pexels-photo-20777176/free-photo-of-model-in-a-red-traditional-dress-and-an-embroidered-scarf.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                alt="sale offer"
-                width={200}
-                height={100}
-              />
-              <div className="flex gap-4 mt-2 text-center">
-                <del className="text-gray-500 dark:text-white">$96</del>
-                <p className="font-bold dark:text-white">${parseInt(96) - 48}</p>
+          </div>
+          {/* Sale Item 3 */}
+          <div className="relative group bg-gray-100 dark:bg-gray-800 rounded-lg shadow-lg overflow-hidden">
+            <Image
+              src="https://images.pexels.com/photos/18571715/pexels-photo-18571715/free-photo-of-child-model-posing-in-bow-tie-and-suspenders.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="sale offer"
+              layout="responsive"
+              width={500}
+              height={300}
+              className="object-cover w-full h-64 transition-transform duration-300 group-hover:scale-110"
+            />
+            <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent p-4">
+              <div className="flex justify-between items-center mb-4">
+                <del className="text-gray-400 dark:text-gray-500">$67</del>
+                <p className="text-2xl font-bold text-white">$33.5</p>
               </div>
-              <button className="mt-2 border border-gray-400 hover:border-transparent px-3 rounded-md dark:text-white hover:bg-red-500 text-black hover:text-white">
-                Shop
+              <button className="w-full py-2 bg-red-500 text-white rounded hover:bg-red-700 transition-colors">
+                Shop Now
               </button>
             </div>
-
-            <div className="flex flex-col items-center max-w-xs sm:max-w-sm">
-              <Image
-                className="w-24 sm:w-32 h-32 object-cover rounded-md"
-                src="https://images.unsplash.com/photo-1578897366846-358bb1c2412a?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt="sale offer"
-                width={400}
-                height={100}
-              />
-              <div className="flex gap-4 mt-2 text-center">
-                <del className="text-gray-500 dark:text-white">$81</del>
-                <p className="font-bold dark:text-white">${parseInt(81 / 2)}</p>
+          </div>
+          {/* Sale Item 4 */}
+          <div className="relative group bg-gray-100 dark:bg-gray-800 rounded-lg shadow-lg overflow-hidden">
+            <Image
+              src="https://images.pexels.com/photos/19664801/pexels-photo-19664801/free-photo-of-little-boy-in-vest-and-white-shirt-standing-on-rock.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="sale offer"
+              layout="responsive"
+              width={500}
+              height={300}
+              className="object-cover w-full h-64 transition-transform duration-300 group-hover:scale-110"
+            />
+            <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent p-4">
+              <div className="flex justify-between items-center mb-4">
+                <del className="text-gray-400 dark:text-gray-500">$100</del>
+                <p className="text-2xl font-bold text-white">$50</p>
               </div>
-              <button className="mt-2 border border-gray-400 hover:border-transparent px-3 rounded-md dark:text-white hover:bg-red-500 text-black hover:text-white">
-                Shop
+              <button className="w-full py-2 bg-red-500 text-white rounded hover:bg-red-700 transition-colors">
+                Shop Now
+              </button>
+            </div>
+          </div>
+          {/* Sale Item 5 */}
+          <div className="relative group bg-gray-100 dark:bg-gray-800 rounded-lg shadow-lg overflow-hidden">
+            <Image
+              src="https://images.pexels.com/photos/20777176/pexels-photo-20777176/free-photo-of-model-in-a-red-traditional-dress-and-an-embroidered-scarf.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="sale offer"
+              layout="responsive"
+              width={500}
+              height={300}
+              className="object-cover w-full h-64 transition-transform duration-300 group-hover:scale-110"
+            />
+            <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent p-4">
+              <div className="flex justify-between items-center mb-4">
+                <del className="text-gray-400 dark:text-gray-500">$96</del>
+                <p className="text-2xl font-bold text-white">$48</p>
+              </div>
+              <button className="w-full py-2 bg-red-500 text-white rounded hover:bg-red-700 transition-colors">
+                Shop Now
+              </button>
+            </div>
+          </div>
+          {/* Sale Item 6 */}
+          <div className="relative group bg-gray-100 dark:bg-gray-800 rounded-lg shadow-lg overflow-hidden">
+            <Image
+              src="https://images.unsplash.com/photo-1578897366846-358bb1c2412a?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="sale offer"
+              layout="responsive"
+              width={500}h
+              height={300}
+              className="object-cover w-full h-64 transition-transform duration-300 group-hover:scale-110"
+            />
+            <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent p-4">
+              <div className="flex justify-between items-center mb-4">
+                <del className="text-gray-400 dark:text-gray-500">$81</del>
+                <p className="text-2xl font-bold text-white">$40.5</p>
+              </div>
+              <button className="w-full py-2 bg-red-500 text-white rounded hover:bg-red-700 transition-colors">
+                Shop Now
+              </button>
+            </div>
+          </div>
+          {/* Sale Item 7 */}
+          <div className="relative group bg-gray-100 dark:bg-gray-800 rounded-lg shadow-lg overflow-hidden">
+            <Image
+              src="https://images.unsplash.com/photo-1585518126763-5146ad8837a0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDk4fHx8ZW58MHx8fHx8"
+              alt="sale offer"
+              layout="responsive"
+              width={500}
+              height={300}
+              className="object-cover w-full h-64 transition-transform duration-300 group-hover:scale-110"
+            />
+            <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent p-4">
+              <div className="flex justify-between items-center mb-4">
+                <del className="text-gray-400 dark:text-gray-500">$130</del>
+                <p className="text-2xl font-bold text-white">$65</p>
+              </div>
+              <button className="w-full py-2 bg-red-500 text-white rounded hover:bg-red-700 transition-colors">
+                Shop Now
+              </button>
+            </div>
+          </div>
+          {/* Sale Item 8 */}
+          <div className="relative group bg-gray-100 dark:bg-gray-800 rounded-lg shadow-lg overflow-hidden">
+            <Image
+              src="https://images.unsplash.com/photo-1620012253295-c15cc3e65df4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDgyfHx8ZW58MHx8fHx8"
+              alt="sale offer"
+              layout="responsive"
+              width={500}
+              height={300}
+              className="object-cover w-full h-64 transition-transform duration-300 group-hover:scale-110"
+            />
+            <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent p-4">
+              <div className="flex justify-between items-center mb-4">
+                <del className="text-gray-400 dark:text-gray-500">$150</del>
+                <p className="text-2xl font-bold text-white">$75</p>
+              </div>
+              <button className="w-full py-2 bg-red-500 text-white rounded hover:bg-red-700 transition-colors">
+                Shop Now
               </button>
             </div>
           </div>
         </div>
-
-        <div className="px-12 text-center">
-          <Image
-            src="https://images.unsplash.com/photo-1710868941935-4ecf76ce8a6d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="sale offer"
-            width={500}
-            height={500}
-            className="object-cover rounded-md hover:blur-sm"
-          />
-
-          <div className="flex gap-4 ">
-            <del className="mt-2 text-center text ml-12 sm:ml-32 dark:text-white ">$112</del>
-            <p className="mt-2 text-center font-bold dark:text-white">${parseInt(112 / 2)}</p>
-          </div>
-          <button className="border border-gray-400  hover:border-transparent px-3 rounded-md dark:text-white hover:bg-red-500 text-black hover:text-white ml-2">
-            Shop
-          </button>
-        </div>
       </div>
-      </div>
-    </>
+    </div>
   );
 }
